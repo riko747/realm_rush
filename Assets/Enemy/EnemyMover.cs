c1e2b83e772371199773476539dacef301830f1e
Vitaliy Bosiy <vitaliybosiy@gmail.com>
2021-04-20 15:55:46 +0300

    -Preparation for enemy movement;

---
+++ Assets/Enemy/EnemyMover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path = new List<WayPoint>();
+    [SerializeField] float waitTime = 1f;
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(WayPoint wayPoint in path)
+        {
+            transform.position = wayPoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
